97d3c84791e5804aa430b2f6ee24251c2961a957
VLages <vitormoraeslages@gmail.com>
2026-03-31 15:35:26 -0300

    Commit

--- editor_grafico.py
+++ editor_grafico.py
@@ -11,7 +11,7 @@
 from PyQt5 import QtCore, QtGui, QtWidgets
 
 
-class Ui_Dialog(object):
+class Ui_editor_grafico(object):
     def setupUi(self, Dialog):
         Dialog.setObjectName("Dialog")
         Dialog.resize(922, 633)
@@ -179,7 +179,7 @@ if __name__ == "__main__":
     import sys
     app = QtWidgets.QApplication(sys.argv)
     Dialog = QtWidgets.QDialog()
-    ui = Ui_Dialog()
+    ui = Ui_editor_grafico()
     ui.setupUi(Dialog)
     Dialog.show()
     sys.exit(app.exec_())
