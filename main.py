26da4e5b3a6b72ddcdc598406bc0d7a6e75b1e44
VLages <vitormoraeslages@gmail.com>
2026-04-01 15:52:08 -0300

    Commit

--- main.py
+++ main.py
@@ -15,7 +15,14 @@ class editor_grafico(QDialog):
         self.ui = Ui_editor_grafico()
         self.ui.setupUi(self)
         self.setWindowFlags(Qt.Window | Qt.WindowMinimizeButtonHint | Qt.WindowMaximizeButtonHint | Qt.WindowCloseButtonHint)
+        self.ui.fullscreembut.clicked.connect(self.tela_cheia)
+        self.ui.grafico
 
+    def tela_cheia(self):
+        if self.isFullScreen():
+            self.showNormal()
+        else:
+            window.showFullScreen()
 
 app = QApplication(sys.argv)
 if (QDialog.Accepted == True):
