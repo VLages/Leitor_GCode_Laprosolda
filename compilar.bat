30bb7174dbab379b43073c007d5ebda3878f5558
VLages <vitormoraeslages@gmail.com>
2026-04-22 15:47:46 -0300

    Commit

---
+++ compilar.bat
@@ -0,0 +1,27 @@
+@echo off
+echo ===================================================
+echo     COMPILANDO O SIMULADOR LAPROSOLDA...
+echo     Isso pode levar alguns minutos. Aguarde.
+echo ===================================================
+echo.
+
+:: 1. Executa o PyInstaller
+C:\Users\Vitor\AppData\Local\Programs\Python\Python313\python.exe -m PyInstaller --noconsole --onefile --hidden-import PyQt5 --hidden-import OpenGL mainGL.py
+
+:: 2. Move o executavel pronto para a pasta principal (e ja renomeia ele)
+echo.
+echo ===================================================
+echo     ORGANIZANDO OS ARQUIVOS...
+echo ===================================================
+move dist\mainGL.exe .\Simulador_Laprosolda.exe
+
+:: 3. Apaga os arquivos e pastas temporarias
+rmdir /s /q build
+rmdir /s /q dist
+del mainGL.spec
+
+echo.
+echo ===================================================
+echo     SUCESSO! O Simulador_Laprosolda.exe esta pronto!
+echo ===================================================
+pause
